190d57b32fb6206e226aa9e18c0b45473b263341
Thomas Polasek <tpolasek@cs.ualberta.ca>
2009-07-30 14:42:10 -0600

    initial source

---
+++ src/edu/ualberta/med/scannerconfig/ScannerSettingsHandler.java
@@ -0,0 +1,17 @@
+package edu.ualberta.med.scannerconfig;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.swt.SWT;
+import org.eclipse.ui.PlatformUI;
+
+public class ScannerSettingsHandler extends AbstractHandler implements IHandler {
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        ConfigDialog configDialog = new ConfigDialog(PlatformUI.getWorkbench()
+            .getActiveWorkbenchWindow().getShell(), SWT.NONE);
+        configDialog.open();
+        return null;
+    }
+}
