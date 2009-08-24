4cab115106a39264d7203ae2835fbe433614956d
Nelson Loyola <loyola@cs.ualberta.ca>
2009-08-24 09:04:52 -0600

    using preferences

--- src/edu/ualberta/med/scannerconfig/preferences/Scanner.java
+++ src/edu/ualberta/med/scannerconfig/preferences/Scanner.java
@@ -19,31 +19,35 @@ public class Scanner extends FieldEditorPreferencePage implements
 
     @Override
     public void createFieldEditors() {
-        RadioGroupFieldEditor rgFe = new RadioGroupFieldEditor(
-            PreferenceConstants.SCANNER_DPI, "DPI", 3, new String[][] {
-                { "300", PreferenceConstants.SCANNER_DPI_300 },
-                { "400", PreferenceConstants.SCANNER_DPI_400 },
-                { "600", PreferenceConstants.SCANNER_DPI_600 } },
-            getFieldEditorParent(), true);
+        RadioGroupFieldEditor rgFe =
+            new RadioGroupFieldEditor(PreferenceConstants.SCANNER_DRV_TYPE,
+                "Driver Type", 2, new String[][] {
+                    { "WIA", PreferenceConstants.SCANNER_DRV_TYPE_WIA },
+                    { "TWAIN", PreferenceConstants.SCANNER_DRV_TYPE_TWAIN } },
+                getFieldEditorParent(), true);
         addField(rgFe);
 
-        IntegerFieldEditor intFe = new IntegerFieldEditor(
-            PreferenceConstants.SCANNER_BRIGHTNESS, "Brigtness",
-            getFieldEditorParent());
+        rgFe =
+            new RadioGroupFieldEditor(PreferenceConstants.SCANNER_DPI, "DPI",
+                3, new String[][] {
+                    { "300", PreferenceConstants.SCANNER_DPI_300 },
+                    { "400", PreferenceConstants.SCANNER_DPI_400 },
+                    { "600", PreferenceConstants.SCANNER_DPI_600 } },
+                getFieldEditorParent(), true);
+        addField(rgFe);
+
+        IntegerFieldEditor intFe =
+            new IntegerFieldEditor(PreferenceConstants.SCANNER_BRIGHTNESS,
+                "Brigtness", getFieldEditorParent());
         intFe.setValidRange(-1000, 1000);
         addField(intFe);
 
-        intFe = new IntegerFieldEditor(PreferenceConstants.SCANNER_CONTRAST,
-            "Contrast", getFieldEditorParent());
+        intFe =
+            new IntegerFieldEditor(PreferenceConstants.SCANNER_CONTRAST,
+                "Contrast", getFieldEditorParent());
         intFe.setValidRange(-1000, 1000);
         addField(intFe);
 
-        rgFe = new RadioGroupFieldEditor(PreferenceConstants.SCANNER_DRV_TYPE,
-            "Driver Type", 2, new String[][] {
-                { "WIA", PreferenceConstants.SCANNER_DRV_TYPE_WIA },
-                { "TWAIN", PreferenceConstants.SCANNER_DRV_TYPE_TWAIN } },
-            getFieldEditorParent(), true);
-        addField(rgFe);
     }
 
     @Override
