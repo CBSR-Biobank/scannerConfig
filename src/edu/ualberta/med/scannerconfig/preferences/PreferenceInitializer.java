bfc3c2e14c04328aaa810854cdf69b7dd662d9a0
Nelson Loyola <loyola@cs.ualberta.ca>
2010-04-15 15:48:57 -0600

    added default values for new decode preferences

--- src/edu/ualberta/med/scannerconfig/preferences/PreferenceInitializer.java
+++ src/edu/ualberta/med/scannerconfig/preferences/PreferenceInitializer.java
@@ -16,6 +16,11 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
         store.setDefault(PreferenceConstants.SCANNER_CONTRAST, 0);
         store.setDefault(PreferenceConstants.SCANNER_DRV_TYPE,
             PreferenceConstants.SCANNER_DRV_TYPE_TWAIN);
+        store.setDefault(PreferenceConstants.LIBDMTX_EDGE_THRESH, 5);
+        store.setDefault(PreferenceConstants.LIBDMTX_SCAN_GAP, 0.075);
+        store.setDefault(PreferenceConstants.LIBDMTX_SQUARE_DEV, 10);
+        store.setDefault(PreferenceConstants.LIBDMTX_CORRECTIONS, 10);
+        store.setDefault(PreferenceConstants.LIBDMTX_CELL_DISTANCE, 0.345);
 
         for (int i = 0; i < PreferenceConstants.SCANNER_PALLET_ENABLED.length; i++) {
             store.setDefault(PreferenceConstants.SCANNER_PALLET_ENABLED[i],
