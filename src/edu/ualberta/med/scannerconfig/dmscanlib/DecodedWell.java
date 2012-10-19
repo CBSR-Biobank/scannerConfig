11255478eb13ba5bebef9de36df61e16ec203d41
Nelson Loyola <nloyola@gmail.com>
2012-10-19 17:15:25 -0600

    implementing new scanning library API

--- src/edu/ualberta/med/scannerconfig/dmscanlib/DecodedWell.java
+++ src/edu/ualberta/med/scannerconfig/dmscanlib/DecodedWell.java
@@ -1,5 +1,7 @@
 package edu.ualberta.med.scannerconfig.dmscanlib;
 
+import edu.ualberta.med.biobank.util.SbsLabeling;
+
 public class DecodedWell implements Comparable<DecodedWell> {
 
     final String label;
@@ -7,6 +9,11 @@ public class DecodedWell implements Comparable<DecodedWell> {
 
     public DecodedWell(String label, String message) {
         this.label = label;
+        this.message = message;
+    }
+
+    public DecodedWell(int row, int col, String message) {
+        this.label = SbsLabeling.fromRowCol(row, col);
         this.message = message;
     }
 
